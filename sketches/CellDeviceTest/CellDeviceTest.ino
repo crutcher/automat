dd43bf67de5da2cd7ebc06d2f94ecfd92219eaaa
Crutcher Dunnavant <crutcher@gmail.com>
2019-10-27 15:59:36 -0700

    wip

---
+++ sketches/CellDeviceTest/CellDeviceTest.ino
@@ -0,0 +1,46 @@
+
+// PINS:
+// LED_BUILTIN
+
+// FIXME(crutcher): We should be able to use IDE constants (D1, D2, etc).
+// But it seems the board-setup-selection is wrong in our env; so
+// instead we're redefining them for the Wemos D1
+const int BUTTON_LED_PIN = 5;
+const int BUTTON_PIN = 4;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("");
+  Serial.println("setup");
+
+  pinMode(BUTTON_LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+bool buttonPressed() {
+  return not digitalRead(BUTTON_PIN);
+}
+
+const long buttonAnimDelay = 100;
+long buttonAnimTimer = 0;
+bool buttonAnimState = false;
+
+void renderButtonLed() {
+  if (!buttonPressed()) {
+    digitalWrite(BUTTON_LED_PIN, LOW);
+    return;
+  }
+
+  long now = millis();
+  if (now - buttonAnimTimer > buttonAnimDelay) {
+    buttonAnimState = !buttonAnimState;
+    buttonAnimTimer = now;
+  }
+
+  digitalWrite(BUTTON_LED_PIN, buttonAnimState);
+}
+
+void loop() {
+  renderButtonLed();
+}
